013adbe891e039535cbe86743db7f3effd992a29
anisH <97351302+AnisH521@users.noreply.github.com>
2022-12-28 20:55:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,12 +22,12 @@ def decompress_pickle(file):
     return data
 
 url = 'https://drive.google.com/uc?id=12yMGBY-8vyoeul94nuNCPkb7F7FrmJVS'
-loaded_vectorizer = '/tmp/vec.pbz2'
+loaded_vectorizer = 'vectorizer/vec.pbz2'
 gdown.download(url, loaded_vectorizer, quiet=False)
 loaded_vectorizer = decompress_pickle(loaded_vectorizer)
 
 url = "https://drive.google.com/uc?id=1_FTgSc6Mc6t93SSaB_xP5KuxAEJ-EqSW"
-loaded_model = '/tmp/model.pbz2'
+loaded_model = 'model/model.pbz2'
 gdown.download(url, loaded_model, quiet=False)
 loaded_model = decompress_pickle(loaded_model)
 
